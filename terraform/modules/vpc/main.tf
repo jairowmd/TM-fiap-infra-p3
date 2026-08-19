51be9612bc2d6aadb1da1dfb419be45d762d5642
jairowmd <jairowmd@hotmail.com>
2026-08-19 18:10:51 -0300

    feat(network): associate public subnets with route table

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -59,3 +59,28 @@ resource "aws_internet_gateway" "this" {
     Environment = var.environment
   }
 }
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-rt"
+    Project     = var.project_name
+    Environment = var.environment
+    Tier        = "Public"
+  }
+}
+
+# Public Route Table Associations
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
