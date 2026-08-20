9cb810c1a5d3fbd96b0995f73dacbeea5dc365df
jairowmd <jairowmd@hotmail.com>
2026-08-19 23:00:27 -0300

    feat(network): add nat gateway for private subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -89,6 +89,11 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
   tags = {
     Name        = "${var.project_name}-${var.environment}-private-rt"
     Project     = var.project_name
@@ -103,4 +108,29 @@ resource "aws_route_table_association" "private" {
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
+}
+
+# Elastic IP para o NAT Gateway - Cria um IP público estático na AWS.
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-nat-eip"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  depends_on = [aws_internet_gateway.this]
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-nat-gateway"
+    Project     = var.project_name
+    Environment = var.environment
+  }
 }
